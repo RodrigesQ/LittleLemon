29dec1b5718979823b5e1b14a2b142a1506b4ffd
RodrigesQ <rodriges@inbox.lv>
2024-05-01 15:24:18 +0300

    4. commit

--- workspace/littlelemon/littlelemon/settings.py
+++ workspace/littlelemon/littlelemon/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'LittleLemon',
         'USER': 'root',
         'PASSWORD': 'Q371LmSP1*0O',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    id = models.CharField(primary_key=True, max_length=5)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    id = models.CharField(primary_key=True, max_length=11)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.CharField(max_length=6)
+    bookingdate = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.name} on {self.bookingdate}"
